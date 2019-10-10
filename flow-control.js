5350234b265ae31fdc30ad014af3acfff27a3f04
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-10 17:51:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,7 +19,7 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
-  elseif (age < 13) {
+  else if (age < 13) {
     return "You are a kid"
   }
   else {
